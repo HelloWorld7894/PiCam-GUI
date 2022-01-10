7d7acc8cdcba705fad5ac1b176e3f4fca11696cc
HelloWorld7894 <59885570+HelloWorld7894@users.noreply.github.com>
2022-01-10 22:06:18 +0100

    Updated with Camera.py

---
+++ src/Camera.py
@@ -0,0 +1,31 @@
+#Imports
+from picamera import PiCamera
+import time as t
+
+#Camera setup
+Cam = PiCamera(sensor_mode=2)
+
+#Basic setup
+Cam.brightness = 50
+Cam.sharpness = 0
+Cam.contrast = 0
+Cam.saturation = 0
+Cam.iso = 0
+Cam.exposure_compensation = 0
+Cam.exposure_mode = "auto"
+Cam.meter_mode = "average"
+Cam.awb_mode = "auto"
+Cam.rotation = 0
+Cam.hflip = False
+Cam.vflip = False
+Cam.crop = (0.0, 0.0, 1.0, 1.0)
+Cam.framerate = 30
+
+def CameraON():
+    Cam.preview_fullscreen = False
+    Cam.preview_window = (160, 120, 80, 60)
+    Cam.resolution = (640, 480)
+    Cam.start_preview()
+
+def CameraOFF():
+    Cam.stop_preview()
--- src/Main.py
+++ src/Main.py
@@ -1,12 +1,30 @@
 from tkinter import *
-from tkinter.ttk import *
 
 #Other imports
 from PIL import ImageTk, Image
+import Camera as C
 
 """
     MAIN WINDOW
 """
+def GetLocation(var, spec_win):
+    #variable override
+    for widget in spec_win.winfo_children():
+        if "radiobutton" not in str(widget):
+            widget.destroy()
+    C.CameraOFF()
+
+    if int(var.get()) == 1: #Photograph mode
+        Header = Label(spec_win, text="Photograph mode", font=("Arial", 25)).grid(row=1, column=2)
+        C.CameraON()
+
+    elif int(var.get()) == 2: #Camera settings
+        Header = Label(spec_win, text="Camera settings", font=("Arial", 25)).grid(row=1, column=2)
+    elif int(var.get()) == 3: #Image processing
+        Header = Label(spec_win, text="Image processing", font=("Arial", 25)).grid(row=1, column=2)
+    elif int(var.get()) == 4: #Images
+        Header = Label(spec_win, text="Images", font=("Arial", 25)).grid(row=1, column=2)
+
 def main():
     # destroy splash window
     LoadingWin.destroy()
@@ -18,14 +36,30 @@ def main():
     MainWin.attributes("-fullscreen", True)
     MainWin.configure(bg="white")
 
+    Var = StringVar(MainWin, "1")
+    values = {"Photo ": "1",
+              "Camera Settings": "2",
+              "Image Processing": "3",
+              "Images": "4"}
+
+    # Loop is used to create multiple Radiobuttons
+    # rather than creating each button separately
+    for Iter, (text, value) in enumerate(values.items()):
+        Radiobutton ( MainWin, text=text, variable=Var,
+                      value=value, indicator=0,
+                      background="light blue", height = 5, width = 15).grid(row= Iter + 1, column=1)
+
+    GetLocation(Var, MainWin)
+    Var.trace("w", lambda *_, VarInstnc=Var: GetLocation(VarInstnc, MainWin))
+
 LoadingWin = Tk(className="CopernicusPi-loader")
 LoadingWin.configure(bg="white")
 
-Logo = Canvas(LoadingWin, width=150, height=150)
+Logo = Canvas(LoadingWin, width=300, height=300)
 Logo.pack()
 
 Img = Image.open("./gui/CopernicusPi.png")
-Resized_Img = Img.resize((150, 150))
+Resized_Img = Img.resize((300, 300))
 Img_Canvas = ImageTk.PhotoImage(Resized_Img)
 Logo.create_image(0, 0, anchor=NW, image=Img_Canvas)
 
