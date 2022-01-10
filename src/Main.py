d6a19da351c0105526fe3648e505287dac5fec4f
HelloWorld7894 <59885570+HelloWorld7894@users.noreply.github.com>
2022-01-10 18:22:39 +0100

    Update Main.py

--- src/Main.py
+++ src/Main.py
@@ -15,21 +15,23 @@ def main():
     MainWin = Tk(className="CopernicusPi-main")
 
     # Adjust size
-    MainWin.geometry("400x400")
-    MainWin.attributes("-fullscreen", True)
+    MainWin.geometry("320x240")
+    #MainWin.attributes("-fullscreen", True)
     MainWin.configure(bg="white")
 
 LoadingWin = Tk(className="CopernicusPi-loader")
-LoadingWin.geometry("200x200")
+LoadingWin.geometry("320x240")
 LoadingWin.configure(bg="white")
 
-Logo = Canvas(LoadingWin, width=500, height=500)
+Logo = Canvas(LoadingWin, width=150, height=150)
 Logo.pack()
 
-Img = ImageTk.PhotoImage(Image.open("./gui/CopernicusPi.png"))
-Logo.create_image(0, 0, anchor=NW, image=Img)
+Img = Image.open("./gui/CopernicusPi.png")
+Resized_Img = Img.resize((150, 150))
+Img_Canvas = ImageTk.PhotoImage(Resized_Img)
+Logo.create_image(0, 0, anchor=NW, image=Img_Canvas)
 
-LoadingWin.attributes("-fullscreen", True)
+#LoadingWin.attributes("-fullscreen", True)
 LoadingWin.after(3000, main)
 
 # Execute tkinter
