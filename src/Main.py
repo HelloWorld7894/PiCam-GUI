7ebf2cf61a6bbfed613921e10c8a99cb05f1bbc9
HelloWorld7894 <59885570+HelloWorld7894@users.noreply.github.com>
2022-01-12 21:58:23 +0100

    updated with OOP

--- src/Main.py
+++ src/Main.py
@@ -9,6 +9,35 @@ import os
 """
     MAIN WINDOW
 """
+class MainWin():
+    def __init__(self, master):
+        self.master = master
+        self.master.attributes("-fullscreen", True)
+        self.configure(bg="white")
+        self.prev_inpt = 1
+
+        Var = StringVar(self.master, "1")
+        values = {"Photo ": "1",
+                  "Camera Settings": "2",
+                  "Image Processing": "3",
+                  "Images": "4"}
+
+        for Iter, (text, value) in enumerate ( values.items () ):
+            Radiobutton (self.master, text=text, variable=Var,
+                          value=value, indicator=0,
+                          background="#0e86d1", height=3, width=15 ).grid ( row=Iter + 1, column=1 )
+
+        GetLocation(Var, MainWin)
+        Var.trace("w", lambda *_, VarInstnc=Var: GetLocation(VarInstnc, MainWin))
+
+    def ButtonCheck(self):
+        inpt = GPIO.input(5)
+        if ((not self.prev_inpt) and inpt):
+            Folder_Len = len ( [name for name in os.listdir ( "." ) if os.path.isfile ( name )] )
+            C.Cam.capture ( "home/pi/Desktop/image" + str ( Folder_Len ) + ".jpg" )
+
+        self.master.after(100, self.ButtonCheck)
+
 def GetLocation(var, spec_win):
     #variable override
     for widget in spec_win.winfo_children():
@@ -19,9 +48,7 @@ def GetLocation(var, spec_win):
     if int(var.get()) == 1: #Photograph mode
         Header = Label(spec_win, text="Photograph mode", font=("Arial", 25), bg="white").grid(row=1, column=2)
         C.CameraON()
-
-        prev_inpt = 1
-        C.ButtonCheck(spec_win, prev_inpt)
+        spec_win.ButtonCheck()
 
     elif int(var.get()) == 2: #Camera settings
         Header = Label(spec_win, text="Camera settings", font=("Arial", 25), bg="white").grid(row=1, column=2)
@@ -30,42 +57,12 @@ def GetLocation(var, spec_win):
     elif int(var.get()) == 4: #Images
         Header = Label(spec_win, text="Images", font=("Arial", 25), bg="white").grid(row=1, column=2)
 
-def ButtonCheck(spec_win, prev_inpt):
-    inpt = GPIO.input(5)
-    if ((not prev_inpt) and inpt):
-        Folder_Len = len([name for name in os.listdir(".") if os.path.isfile(name)])
-        C.Cam.capture("home/pi/Desktop/image" + str(Folder_Len) + ".jpg")
-
-    spec_win.after(100, ButtonCheck(spec_win, inpt))
-    #Folder_Len = len([name for name in os.listdir(".") if os.path.isfile(name)])
-    #Cam.capture("home/pi/CopernicusPi/saved/image" + str(Folder_Len))
-
 def main():
     # destroy splash window
     LoadingWin.destroy()
 
     # Execute tkinter
-    MainWin = Tk(className="CopernicusPi-main")
-
-    # Adjust size
-    MainWin.attributes("-fullscreen", True)
-    MainWin.configure(bg="white")
-
-    Var = StringVar(MainWin, "1")
-    values = {"Photo ": "1",
-              "Camera Settings": "2",
-              "Image Processing": "3",
-              "Images": "4"}
-
-    # Loop is used to create multiple Radiobuttons
-    # rather than creating each button separately
-    for Iter, (text, value) in enumerate(values.items()):
-        Radiobutton ( MainWin, text=text, variable=Var,
-                      value=value, indicator=0,
-                      background="#0e86d1", height = 3, width = 15).grid(row= Iter + 1, column=1)
-
-    GetLocation(Var, MainWin)
-    Var.trace("w", lambda *_, VarInstnc=Var: GetLocation(VarInstnc, MainWin))
+    Global_Win = MainWin(Tk(className="CopernicusPi-main"))
 
 #Button setup
 GPIO.setmode(GPIO.BCM)
